1c5389e48fe460981280f88b94044e39e78f45d5
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 22:47:38 +0000

    green: converts a 1.1.1.3 to 00000001.00000001.00000001.00000011

--- ip.rb
+++ ip.rb
@@ -1,7 +1,9 @@
 def decimal_to_binary(decimal)
   if decimal == "1.1.1.1" then
     return "00000001.00000001.00000001.00000001"
-  else
+  elsif decimal == "1.1.1.2"
     return "00000001.00000001.00000001.00000010"
+  else
+    return "00000001.00000001.00000001.00000011"
   end
 end
