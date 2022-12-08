0cc764690645f589a6115b7b7d52e428bc940978
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 22:42:53 +0000

    red: converts a 1.1.1.2 to 00000001.00000001.00000001.00000010

--- spec/ip_spec.rb
+++ spec/ip_spec.rb
@@ -1,7 +1,10 @@
 require './ip.rb'
 
 describe 'decimal_to_binary' do 
-  it 'converts a decimal ip to a binary ip' do 
+  it 'converts a "1.1.1.1" to "00000001.00000001.00000001.00000001"' do 
     expect(decimal_to_binary("1.1.1.1")).to eq("00000001.00000001.00000001.00000001")
+  end
+  it 'converts a "1.1.1.2" to "00000001.00000001.00000001.00000010"' do 
+    expect(decimal_to_binary("1.1.1.2")).to eq("00000001.00000001.00000001.00000010")
   end
 end
