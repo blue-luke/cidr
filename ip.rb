ba23c64eac04cd99f19801187dd0f07402e25fa4
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 23:02:48 +0000

    refactor: converts a 1.1.1.3 to 00000001.00000001.00000001.00000011

--- ip.rb
+++ ip.rb
@@ -1,9 +1,4 @@
 def decimal_to_binary(decimal)
-  if decimal == "1.1.1.1" then
-    return "00000001.00000001.00000001.00000001"
-  elsif decimal == "1.1.1.2"
-    return "00000001.00000001.00000001.00000010"
-  else
-    return "00000001.00000001.00000001.00000011"
-  end
+  array = decimal.split(".")
+  return array.map { |n| n.to_i.to_s(2) }.join(".")
 end
