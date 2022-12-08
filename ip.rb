4e2e98002d922afd31dcb222702e3647226bfab3
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 22:41:28 +0000

    refactor: syntax error

--- ip.rb
+++ ip.rb
@@ -1,3 +1,3 @@
 def decimal_to_binary(decimal)
-  return "00000001.0000001.00000001.00000001"
+  return "00000001.00000001.00000001.00000001"
 end
--- spec/ip_spec.rb
+++ spec/ip_spec.rb
@@ -2,6 +2,6 @@ require './ip.rb'
 
 describe 'decimal_to_binary' do 
   it 'converts a decimal ip to a binary ip' do 
-    expect(decimal_to_binary("1.1.1.1")).to eq("00000001.0000001.00000001.00000001")
+    expect(decimal_to_binary("1.1.1.1")).to eq("00000001.00000001.00000001.00000001")
   end
 end
