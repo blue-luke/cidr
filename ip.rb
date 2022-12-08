0f241f20fbd437d1569f65522a193eea289a7d0e
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 23:15:51 +0000

    refactor: converts a 1.1.1.3 to 00000001.00000001.00000001.00000011

--- ip.rb
+++ ip.rb
@@ -1,4 +1,14 @@
 def decimal_to_binary(decimal)
-  array = decimal.split(".")
-  return array.map { |n| n.to_i.to_s(2) }.join(".")
+  binary_array_formatted = []
+  decimal_array = decimal.split(".")
+  binary_string = decimal_array.map { |n| n.to_i.to_s(2) }.join(".")
+  binary_array = binary_string.split(".")
+  binary_array.each do |n|
+    zeroes = 8 - n.length
+    zeroes.times do
+      n.prepend("0")
+    end
+  binary_array_formatted << n
+  end
+  return binary_array_formatted.join(".")
 end
