64477b59b6ed216b38ca0fbb0d6c57e12b27222b
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-08 23:26:29 +0000

    red: returns 0 when passed x.x.x.x/32

--- spec/ip_spec.rb
+++ spec/ip_spec.rb
@@ -11,3 +11,9 @@ describe 'decimal_to_binary' do
     expect(decimal_to_binary("1.1.1.3")).to eq("00000001.00000001.00000001.00000011")
   end
 end
+
+describe 'enumerate_ips' do
+  it 'returns 0 when passed "x.x.x.x/32"' do
+    expect(enumerate_ips("x.x.x.x/32")).to eq("0")
+  end
+end
